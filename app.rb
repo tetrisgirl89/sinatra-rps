78fe855037ec7b2b3aaef93d8ad6f31f7d0d2a64
tetrisgirl89 <146213633+tetrisgirl89@users.noreply.github.com>
2023-10-18 16:05:51 +0000

    save

--- app.rb
+++ app.rb
@@ -2,26 +2,65 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:elephant)
 end
 
 get("/rock") do
-   random= rand(1..3)
-  if random = 1
+   random = rand(1..3)
+  if random == 1
 they_played = "rock"
 result = "tied"
-  elsif random = 2
-hey_played = "paper"
+  elsif random == 2
+they_played = "paper"
 result = "lost"
-  elsif random = 2
-  hey_played = "scissors"
+  elsif random == 3
+  they_played = "scissors"
   result = "won"
+end
 
-
-  @outcome = "We played rock! They played #{they_played}! We #{result}!" 
+  @outcome1 = "We played rock!" 
+  @outcome2 = "They played #{they_played}!"
+  @outcome3 =  "We #{result}!"
 
   erb(:play_rock)
+end
+
+get("/paper") do
+  random = rand(1..3)
+ if random == 1
+they_played = "paper"
+result = "tied"
+ elsif random == 2
+they_played = "scissors"
+result = "lost"
+ elsif random == 3
+ they_played = "rock"
+ result = "won"
+end
+
+ @outcome1 = "We played paper!" 
+ @outcome2 = "They played #{they_played}!"
+ @outcome3 =  "We #{result}!"
+
+ erb(:play_paper)
+end
+
+get("/scissors") do
+  random = rand(1..3)
+ if random == 1
+they_played = "scissors"
+result = "tied"
+ elsif random == 2
+they_played = "rock"
+result = "lost"
+ elsif random == 3
+ they_played = "paper"
+ result = "won"
+end
+
+ @outcome1 = "We played scissors!" 
+ @outcome2 = "They played #{they_played}!"
+ @outcome3 =  "We #{result}!"
+
+ erb(:play_paper)
 end
