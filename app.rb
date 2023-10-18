7b2f0af16813744ff4440540fffb4643304d61d1
tetrisgirl89 <146213633+tetrisgirl89@users.noreply.github.com>
2023-10-18 15:27:16 +0000

    save

--- app.rb
+++ app.rb
@@ -7,3 +7,21 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+   random= rand(1..3)
+  if random = 1
+they_played = "rock"
+result = "tied"
+  elsif random = 2
+hey_played = "paper"
+result = "lost"
+  elsif random = 2
+  hey_played = "scissors"
+  result = "won"
+
+
+  @outcome = "We played rock! They played #{they_played}! We #{result}!" 
+
+  erb(:play_rock)
+end
